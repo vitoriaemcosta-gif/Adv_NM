76a1b912e97a73b3c7e349c2d40d0249ba284e85
Vitoria Emanuelly Machado Costa <vitoriae.m.costa@gmail.com>
2026-06-10 12:41:09 -0300

    first commit

---
+++ adv.js
@@ -0,0 +1,21 @@
+// Efeito de envio no formulário
+document.querySelector('.form-contato').addEventListener('submit', function() {
+    const btn = document.querySelector('.btn-submit');
+    btn.innerHTML = "Mensagem Enviada!";
+    btn.style.background = "#25d366";
+});
+
+// NavBar Responsiva
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+// Fecha o menu ao clicar em qualquer link (opcional, mas recomendado)
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
