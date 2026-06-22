78a55d3875fa3dbbbf40f6d22081c12a717e31a6
Vitoria Emanuelly Machado Costa <vitoriae.m.costa@gmail.com>
2026-06-22 12:39:04 -0300

    atualizações, navbar e contato

--- adv.js
+++ adv.js
@@ -1,3 +1,16 @@
+// Links de navegação ativos
+const links = document.querySelectorAll('.nav-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            // Remove a classe 'active' do link que estava ativo antes
+            document.querySelector('.nav-link.active').classList.remove('active');
+            
+            // Adiciona a classe 'active' no link que acabou de ser clicado
+            this.classList.add('active');
+        });
+    });
+
 // Efeito de envio no formulário
 document.querySelector('.form-contato').addEventListener('submit', function() {
     const btn = document.querySelector('.btn-submit');
